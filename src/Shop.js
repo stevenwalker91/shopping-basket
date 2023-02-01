8993257c0992beec6df8ecd6f5b61837a7e5f42a
Steven Walker <stevenwalker1991@googlemail.com>
2023-02-01 15:43:40 +0000

    added function to pull categories when the shop is loaded

--- src/Shop.js
+++ src/Shop.js
@@ -1,6 +1,31 @@
+import {useState, useEffect} from 'react';
+import { v4 as uuidv4 } from 'uuid';
+
 const Shop = () => {
+
+  const [categories, setCategories] = useState([]);
+ 
+  const getShoppingCategories = async () => {
+    const result = await fetch(`https://dummyjson.com/products/categories`);
+    const categories = await result.json();
+    setCategories(categories);
+  }
+
+  useEffect(() => {
+    getShoppingCategories();
+  },[])
+
   return (
-    <div>Shop</div>
+    <div>
+      <h1>Shop</h1>
+      <ul>
+      {
+      categories.map((item) => {
+        return <li key={uuidv4()}>{item}</li>
+      })
+      }
+      </ul>
+    </div>
   )
 }
 
