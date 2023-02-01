7b450e0e80cedc42457f677453f02f16cf109704
Steven Walker <stevenwalker1991@googlemail.com>
2023-02-01 16:18:42 +0000

    Added some styling to category names. added loader that will show when request is being made.

--- src/Shop.js
+++ src/Shop.js
@@ -4,25 +4,29 @@ import { v4 as uuidv4 } from 'uuid';
 const Shop = () => {
 
   const [categories, setCategories] = useState([]);
+  const [isLoading, setIsLoading] = useState([true])
+
  
   const getShoppingCategories = async () => {
     const result = await fetch(`https://dummyjson.com/products/categories`);
     const categories = await result.json();
     setCategories(categories);
+    setIsLoading(false)
   }
 
   useEffect(() => {
     getShoppingCategories();
-  },[])
+  },[isLoading])
 
   return (
     <div>
       <h1>Shop</h1>
       <ul>
-      {
-      categories.map((item) => {
-        return <li key={uuidv4()}>{item}</li>
-      })
+      { !isLoading ? 
+        categories.map((item) => {
+          return <li className="category-list-name" key={uuidv4()}>{item.replace('-', ' ')}</li>
+        })
+        : 'loading' // to be replaced with loader.js
       }
       </ul>
     </div>
